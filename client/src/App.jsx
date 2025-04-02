716e9d27fe73883702cf7304d88a8d85e0c09d8b
cootan <glitchxabhay@gmail.com>
2025-04-03 00:22:34 +0530

    landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ModernEmergencyGuideLP from './components/landing-page';
+
 import Home from './components/landing-page';
-// import LandingPage from './components/Landing-page'; 
 
 const App = () => {
   return (
     <div className="">
 <Home />
-dfd
+
     </div>
   );
 };
